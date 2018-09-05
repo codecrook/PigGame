e89ce10ad578752f34f3ba5a1748689e354973dc
codecrook <hello@codecrook.io>
2018-09-06 01:21:47 +0530

    Added functionality to update currentScore
    What's Done:
    
    - Added code to update currentScore as the dice rolls, for curently active player
    - Current score gets updated when dice is rolled each time adding the diceValue to it.
    - Displays changes on UI
    - Changes the activePlayer to other one when currently activePlayer rolls an 1
    - Adds visual changes to show which player is currently activePlayer
    - Intializes everything to 0 also displays the changes on UI
    - Hides the dice giving the currently activePlayer a blank space to roll on :p

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@
     let scores, roundScore, activePlayer, gamePlaying;
 
     //initiall conditions
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
     firstPlayerTotalScore.textContent = '0';
     secondPlayerTotalScore.textContent = '0';
 
@@ -33,8 +37,33 @@
     rollButton.addEventListener('click', (e) => {
         let diceValue = Math.floor(Math.random() * 6) + 1;
 
+        //code to roll the dice
         dice.style.display = 'block';
         dice.src = `../img/dice-${diceValue}.png`;
+
+        //code to update the current score of player UNTILL he/she rolls an 1
+
+        if (diceValue !== 1) {
+            //add score for current player
+            roundScore += diceValue;
+            document.querySelector(`#current-${activePlayer}`).textContent = roundScore; //display changes on the page
+        } else {
+            //if the current players rolls 1
+            //move to next player
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //toggle the activePlayer
+            roundScore = 0; //initializes the round score to 0 for current player
+
+            //display the initialized current score as 0 for both the players, on the UI
+            firstPlayerCurrentScore.textContent = '0'; 
+            secondPlayerCurrentScore.textContent = '0';
+
+            //add visual effect fro showing which player is currently active
+            firstPlayerPanel.classList.toggle('active');
+            secondPlayerPanel.classList.toggle('active');
+
+            //hide the dice giving the current player a blank place to roll on :P
+            dice.style.display = 'none';
+        }
     });
 
 })();
