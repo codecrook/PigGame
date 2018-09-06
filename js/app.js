308731e68f1792bdba647a399797aac19cea9f70
codecrook <hello@codecrook.io>
2018-09-06 12:52:21 +0530

    Fix: game continues even after a player has won
    What's Done:
    - Earlier the game was continuing even after one player has won
    - Added a state variable to check when currently the gamePlaying or not
    - Set the gamePlaying to true initially and false if one player has won
    - Now the game stop functioning after a player has won untill we hit the 'new game' button

--- js/app.js
+++ js/app.js
@@ -26,46 +26,53 @@
 
     //adding functionality for roll button
     rollButton.addEventListener('click', (e) => {
-        let diceValue = Math.floor(Math.random() * 6) + 1;
+        if (gamePlaying) {
 
-        //code to roll the dice
-        dice.style.display = 'block';
-        dice.src = `../img/dice-${diceValue}.png`;
+            let diceValue = Math.floor(Math.random() * 6) + 1;
 
-        //code to update the current score of player UNTILL he/she rolls an 1
+            //code to roll the dice
+            dice.style.display = 'block';
+            dice.src = `../img/dice-${diceValue}.png`;
 
-        if (diceValue !== 1) {
-            //add score for current player
-            roundScore += diceValue;
-            document.querySelector(`#current-${activePlayer}`).textContent = roundScore; //display changes on the page
-        } else {
-            //if the current players rolls 1
-            //move to next player
-            changePlayer();
+            //code to update the current score of player UNTILL he/she rolls an 1
+
+            if (diceValue !== 1) {
+                //add score for current player
+                roundScore += diceValue;
+                document.querySelector(`#current-${activePlayer}`).textContent = roundScore; //display changes on the page
+            } else {
+                //if the current players rolls 1
+                //move to next player
+                changePlayer();
+            }
         }
     });
 
     //adding functionality for hold button
     holdButton.addEventListener('click', (e) => {
-        //add roundScore of currently activePlayer to its total score
-        scores[activePlayer] += roundScore;
+        if (gamePlaying) {
+            //add roundScore of currently activePlayer to its total score
+            scores[activePlayer] += roundScore;
 
-        //display the score on UI
-        document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+            //display the score on UI
+            document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
 
-        //check wheather the player has won the game, if not change the activePlayer to next player
-        if (scores[activePlayer] >= 20) { //the player has won
-            //change the name of the activePlayer to WINNER
-            document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
-            //hide the dice
-            dice.style.display = 'none';
-            //add the 'winner' class to activePlayer's pannnel to make visual change
-            document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
-            //remove the 'active' class
-            document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
-        } else {
-            //change the currently activePlayer to next player
-            changePlayer();
+            //check wheather the player has won the game, if not change the activePlayer to next player
+            if (scores[activePlayer] >= 20) { //the player has won
+                //change the name of the activePlayer to WINNER
+                document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+                //hide the dice
+                dice.style.display = 'none';
+                //add the 'winner' class to activePlayer's pannnel to make visual change
+                document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+                //remove the 'active' class
+                document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+                //stop the game-play i.e set the gamePlaying to false
+                gamePlaying = false;
+            } else {
+                //change the currently activePlayer to next player
+                changePlayer();
+            }
         }
     });
 
@@ -98,6 +105,7 @@
      *shows visual changes to UI making it display all the initial conditions
     */
     function initGame() {
+        gamePlaying = true;
         scores = [0, 0];
         roundScore = 0;
         activePlayer = 0;
